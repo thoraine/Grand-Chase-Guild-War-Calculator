1fcbd85666cd8358cfda2309d943a277f50be2b7
thoraine <thoraine@gmail.com>
2019-05-01 19:25:14 +0800

    Use placeholders instead of hardcoded values. Display results of match.

--- calcScript.js
+++ calcScript.js
@@ -1,22 +1,22 @@
 function calcu(){
 
-	var gMaxPoint 		= document.getElementsByName("gScore")[0].value,
-		eStarSurvived1 	= document.getElementsByName("eStarSurvived1")[0].value,
-		eStarSurvived4 	= document.getElementsByName("eStarSurvived4")[0].value, 
-		eStarSurvived6 	= document.getElementsByName("eStarSurvived6")[0].value, 
-		eStarSurvived11 = document.getElementsByName("eStarSurvived11")[0].value,
-		eStarSurvived21 = document.getElementsByName("eStarSurvived21")[0].value,
-		gChance 		= document.getElementsByName("gChance")[0].value;
+	var gMaxPoint 		= document.getElementsByName("gScore")[0].value || "0",
+		eStarSurvived1 	= document.getElementsByName("eStarSurvived1")[0].value || "0",
+		eStarSurvived4 	= document.getElementsByName("eStarSurvived4")[0].value || "0", 
+		eStarSurvived6 	= document.getElementsByName("eStarSurvived6")[0].value || "0", 
+		eStarSurvived11 = document.getElementsByName("eStarSurvived11")[0].value || "0",
+		eStarSurvived21 = document.getElementsByName("eStarSurvived21")[0].value || "0",
+		gChance 		= document.getElementsByName("gChance")[0].value || "0";
 
 	var eStar = [eStarSurvived1, eStarSurvived4, eStarSurvived6, eStarSurvived11, eStarSurvived21];
 
-	var eMaxPoint 		= document.getElementsByName("eScore")[0].value,
-		gStarSurvived1 	= document.getElementsByName("gStarSurvived1")[0].value,
-		gStarSurvived4 	= document.getElementsByName("gStarSurvived4")[0].value, 
-		gStarSurvived6 	= document.getElementsByName("gStarSurvived6")[0].value, 
-		gStarSurvived11 = document.getElementsByName("gStarSurvived11")[0].value,
-		gStarSurvived21 = document.getElementsByName("gStarSurvived21")[0].value,
-		eChance 		= document.getElementsByName("eChance")[0].value;
+	var eMaxPoint 		= document.getElementsByName("eScore")[0].value || "0",
+		gStarSurvived1 	= document.getElementsByName("gStarSurvived1")[0].value || "0",
+		gStarSurvived4 	= document.getElementsByName("gStarSurvived4")[0].value || "0", 
+		gStarSurvived6 	= document.getElementsByName("gStarSurvived6")[0].value || "0", 
+		gStarSurvived11 = document.getElementsByName("gStarSurvived11")[0].value || "0",
+		gStarSurvived21 = document.getElementsByName("gStarSurvived21")[0].value || "0",
+		eChance 		= document.getElementsByName("eChance")[0].value || "0";
 
 	var gStar = [gStarSurvived1, gStarSurvived4, gStarSurvived6, gStarSurvived11, gStarSurvived21];
 
@@ -64,7 +64,12 @@ function calcu(){
 	eMaxPoint = parseInt(eMaxPoint) + (eChance * 15);
 
 	document.getElementsByName("gMaxPoint")[0].value = gMaxPoint;
-
 	document.getElementsByName("eMaxPoint")[0].value = eMaxPoint;
 
+	if (gMaxPoint == eMaxPoint){
+		document.getElementsByName("results")[0].innerHTML = "Draw?";}
+	if (gMaxPoint > eMaxPoint){
+		document.getElementsByName("results")[0].innerHTML = "Winning by "+ (gMaxPoint - eMaxPoint) + " points.";}
+	if (gMaxPoint < eMaxPoint){
+		document.getElementsByName("results")[0].innerHTML = "Losing by "+ (eMaxPoint - gMaxPoint) + " points.";}
 }
